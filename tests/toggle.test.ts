5ae8145294ca1c17243c241265daa75c10671c03
Antoine Delègue <delegue.antoine@gmail.com>
2025-07-26 17:16:38 +0200

    :test_tube: add a test for toggle

---
+++ tests/toggle.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "vitest";
+import { screen } from "shadow-dom-testing-library";
+import { click, render } from "./testingUtils";
+import "@testing-library/jest-dom/vitest";
+import "../examples/toggle.mts";
+
+/**
+ * @jest-environment jsdom
+ */
+describe("Toggle Component", () => {
+    it("should toggle the state", async () => {
+        await render(`<toggle-example></toggle-example>`);
+
+        const btn = screen.getByShadowText("Option 1");
+        await click(btn);
+
+        expect(btn).toHaveClass("selected");
+
+        const btn2 = screen.getByShadowText("Option 2");
+        await click(btn2);
+
+        expect(btn2).toHaveClass("selected");
+    });
+});
